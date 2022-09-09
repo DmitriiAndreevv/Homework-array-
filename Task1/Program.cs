23c9c9da6b3b383c0e40ab96039a4a79cec65eef
dmitriy.andreev.1995@inbox.ru <dmitriy.andreev.1995@inbox.ru>
2022-09-09 20:48:14 +0300

    Homework_array

---
+++ Task1/Program.cs
@@ -0,0 +1,60 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3*/
+
+Console.Clear();
+
+Console.WriteLine("Введите числа M через запятую :");
+int [] numbers = StringToNum(Console.ReadLine());
+
+
+int count = 0;
+
+for (int i = 1; i < numbers.Length; i++)
+{
+    if(numbers[i] >= 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"кол-во чисел больше 0 = {count}");
+
+int[] StringToNum(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input [i] != ',')
+        {
+        if(i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += input [i].ToString();
+            break;
+        }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,69 @@
+﻿/*Дополнительная задача (задача со звёздочкой): Напишите программу, которая задаёт массив из n элементов, которые необходимо заполнить случайными значениями и сдвинуть элементы массива влево, или вправо на 1 позицию.
+
+[8, 5, 1, 7, 0] - [5, 1, 7, 0, 8] - сдвиг влево
+
+[8, 5, 1, 7, 0] - [0, 8, 5, 1, 7] - сдвиг вправо*/
+
+Console.Clear();
+
+Console.WriteLine("Введите размерность массива: ");
+int size = int.Parse(Console.ReadLine());
+
+int [] array = FillArray(size, 1, 10);
+
+Console.Write("Входной массив: ");
+Console.Write("[");
+Console.Write(String.Join(", ", array));
+Console.Write("]");
+
+Console.WriteLine();
+Console.Write("сдвиг влево :   ");
+Console.Write("[");
+Console.Write(String.Join(", ", LeftArray(array))); 
+Console.Write("]");
+
+Console.WriteLine();
+Console.Write("сдвиг вправо :  ");
+Console.Write("[");
+Console.Write(String.Join(", ", RightArray(array))); 
+Console.Write("]");
+
+
+int [] FillArray(int size, int min, int max){
+    int [] result = new int[size];
+         
+    for(int i = 0; i < result.Length;i++)
+    {
+         result[i] = new Random().Next(min, max + 1);    
+    }
+    return result;
+}
+
+int[] LeftArray(int [] array)
+{
+    int[] leftNumbers = new int[array.Length];
+    int last = array[0];
+    for (int i = 1; i <= array.Length - 1; i++)
+    {
+        leftNumbers[i - 1] = array[i];
+    }
+ 
+    leftNumbers[leftNumbers.Length - 1] = last;
+    return leftNumbers;
+}
+
+int [] RightArray(int [] array)
+{
+    int [] rightNumbers = new int[array.Length];
+    int first = array[array.Length - 1];
+
+    for (int j = array.Length - 1;j > 0; j--)
+    {
+       
+        array[j] = array[j - 1];
+    }
+    array[0] = first;
+
+    rightNumbers[rightNumbers.Length - 1] = first;
+    return array;
+}
